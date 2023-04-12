04f77a4b616a8eb6754aaf771f55124b4730dc97
yousra-alt <123966037+yousra-alt@users.noreply.github.com>
2023-04-12 09:45:10 +0200

    Kan sætte højden på 1 søjle ud fra værdien i et array

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
-"use strict"
+"use strict";
 
-window.addEventListener("load", start)
+window.addEventListener("load", start);
 
-function start () {
-console.log("Hello");
+const data = 
+  [
+    10, 17, 23, 8, 30, 3, 28, 12, 13, 26, 0, 13, 21, 30, 17, 6, 18, 1, 20, 8,
+    21, 11, 19, 14, 19, 3, 7, 28, 9, 9, 15, 31, 24, 6, 12, 0, 17, 6, 30, 28, 31,
+  ];
 
-const bars = document.querySelectorAll(".bar")
-// querySelectorAll vælger alle div class.
-const fristBar = bars[0];
-// her udvælger man hvilken bar det er.
 
-let h =40;
-// udvælger højde
-fristBar.style.height= `${h}px`;
+function start() {
+  console.log("Hello");
+
+  const bars = document.querySelectorAll(".bar");
+  // querySelectorAll vælger alle div class.
+  const fristBar = bars[0];
+  // her udvælger man hvilken bar det er.
+
+  let h = data[0] / 32 * 100;
+  // udvælger højde
+  fristBar.style.height = `${h}px`;
 }
