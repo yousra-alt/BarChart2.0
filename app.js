32a2dafa5c5d4500f4c7663cb1a44cd472187991
yousra-alt <123966037+yousra-alt@users.noreply.github.com>
2023-04-12 09:55:53 +0200

    display all bars virker og kan vise array af data

--- app.js
+++ app.js
@@ -2,22 +2,26 @@
 
 window.addEventListener("load", start);
 
-const data = 
-  [
-    10, 17, 23, 8, 30, 3, 28, 12, 13, 26, 0, 13, 21, 30, 17, 6, 18, 1, 20, 8,
-    21, 11, 19, 14, 19, 3, 7, 28, 9, 9, 15, 31, 24, 6, 12, 0, 17, 6, 30, 28, 31,
-  ];
-
+const data = [
+17, 23, 8, 30, 3, 28, 12, 13, 26, 0, 13, 21, 30, 17, 6, 18, 1, 20, 8, 21,
+  11, 19, 14, 19, 3, 7, 28, 9, 9, 15, 31, 24, 6, 12, 0, 17, 6, 30, 28, 31, 3
+];
 
 function start() {
   console.log("Hello");
 
+  displayAllBars()
+}
+
+function displayAllBars() {
   const bars = document.querySelectorAll(".bar");
   // querySelectorAll vælger alle div class.
-  const fristBar = bars[0];
   // her udvælger man hvilken bar det er.
 
-  let h = data[0] / 32 * 100;
-  // udvælger højde
-  fristBar.style.height = `${h}px`;
+  for (let i = 0; i <40; i++) {
+      const bar = bars[i];
+    let h = data[i] / 32 * 100;
+    // udvælger højde
+    bar.style.height = `${h}px`;
+  }
 }
