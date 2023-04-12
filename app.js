82c5485cdbaa00bf0531d31d29c06469182f0a45
yousra-alt <123966037+yousra-alt@users.noreply.github.com>
2023-04-12 09:33:55 +0200

    kan ændre højde på første søjle

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 "use strict"
 
+window.addEventListener("load", start)
+
+function start () {
+console.log("Hello");
+
+const bars = document.querySelectorAll(".bar")
+// querySelectorAll vælger alle div class.
+const fristBar = bars[0];
+// her udvælger man hvilken bar det er.
+
+let h =40;
+// udvælger højde
+fristBar.style.height= `${h}px`;
+}
